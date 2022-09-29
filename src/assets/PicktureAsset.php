71281d3f8bc8827c9efc629fc0db16ae6606250d
Kerri Cranwell <kerri@imarc.com>
2022-09-29 16:54:42 -0400

    Add CpAsset dependencies

--- src/assets/PicktureAsset.php
+++ src/assets/PicktureAsset.php
@@ -3,12 +3,17 @@
 namespace Imarc\Pickture\assets;
 
 use craft\web\AssetBundle;
+use craft\web\assets\cp\CpAsset;
 
 class PicktureAsset extends AssetBundle
 {
     public function init()
     {
         $this->sourcePath = "@Imarc/Pickture/resources";
+
+        $this->depends = [
+            CpAsset::class,
+        ];
 
         $this->css = [
             'css/pickture.css',
